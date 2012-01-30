6f7eea77f3d24cdc5a1cb00ea2c461eb49e8d9b3
adixime <adixime@gmail.com>
2012-01-30 11:58:45 -0500

    got quite far

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $possible_languages = array(
 );
 
 $errors = array();
+$display_thanks = false;
 
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -14,7 +15,8 @@ $username = filter_input(INPUT_POST,'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 $preferredlang = filter_input(INPUT_POST, 'preferredlang', FILTER_SANITIZE_STRING);
 $message = filter_input(INPUT_POST,'message', FILTER_SANITIZE_STRING);
-$acceptterms = filter_input(INPUT_POST, 'acceptterms', FILTER_SANITIZE_STRING);
+//$acceptterms = filter_input(INPUT_POST, 'acceptterms', FILTER_FILTER_DEFAULT);
+$terms = filter_input(INPUT_POST, 'terms', FILTER_DEFAULT);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Check to see if the form has been submitted before validating
 	if (empty($name)) {
@@ -38,9 +40,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Check to see if the form has been
 	if (mb_strlen($message) < 25) { // mb_strlen = multi-byte string length
 		$errors['message'] = true;
 	}
-	if ($acceptterms !== '') {
+	/*if ($acceptterms !== '') {
 		$errors['acceptterms'] = true;
+	}*/
+	/*if(!isset($_POST['terms'])){
+		$errors['terms']=true;
+	}*/
+	if (empty($terms)) {
+		$errors['terms'] = true;
 	}
+	
+	if (empty($errors)){
+		$display_thanks = true;
+	}
+	
+	
 	$thankyou = "Thank you for filling the form.";
 	
 }
@@ -54,7 +68,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Check to see if the form has been
 </head>
 <body>
 	
-	<form method="post" action="index.php">
+    <!--< ?php if (isset($_REQUEST['email'])){
+		$subject = "You are Registered!";
+		$from = "gupt0040@algonquinlive.com";
+		$message = "You have received this mail because you have registered with:";
+		$header = "Aditya Gupta" . $from;
+  	 	mail($email, $subject, $message, $header);
+  		echo "Please check your mailbox.";
+  	}
+	? > -->
+    
+    <?php if ($display_thanks) : ?>
+    
+    	<Strong>Thanks!</Strong>
+        
+    <?php else: ?>   
+    
+    <form method="post" action="index.php">
 		<div>
 			<label for="name">Name<?php if (isset($errors['name'])) : ?> <strong>is required</strong><?php endif; ?></label>
 			<input id="name" name="name" value="<?php echo $name; ?>" required>
@@ -84,22 +114,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Check to see if the form has been
 			<textarea id="message" name="message" required><?php echo $message; ?></textarea>
 		</div>
 		<div>
-	        <label for="acceptterms">I have read and accept the Terms & Conditions<?php if (isset($errors['acceptterms'])) : ?> <strong>Please check the box!</strong><?php endif; ?></label>
-			<input type="checkbox" id="acceptterms" name="acceptterms" value="<?php echo $acceptterms; ?>" required>
+	        <!--<label for="acceptterms">I have read and accept the Terms & Conditions< ?php if (isset($errors['acceptterms'])) : ?> <strong>Please check the box!</strong>< ?php endif; ?></label>
+			<input type="checkbox" id="acceptterms" name="acceptterms" value="< ?php echo $acceptterms; ?>" required> -->
+            <input type="checkbox" id="terms" name="terms" <?php if (!empty($terms)) {echo 'checked'; } ?>>
+            <label for="terms">Accept Terms?</label> 
+			<?php if (isset($errors['terms'])) :?><strong>You must Comply</strong><?php endif; ?>
         </div>
 		<div>
-			<input type="submit" name="formSubmit" value="Submit">
+			<button type="submit">Send Message</button>
         </div>
 	</form>
+    <?php endif; ?>
     
-    <?php if (isset($_REQUEST['email'])){
-		$subject = "You are Registered!";
-		$from = "gupt0040@algonquinlive.com";
-		$message = "You have received this mail because you have registered with:";
-		$header = "Aditya Gupta" . $from;
-  	 	mail($email, $subject, $message, $header);
-  		echo "Please check your mailbox.";
-  	}
-	?>
 </body>
 </html>
